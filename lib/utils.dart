42966ba38f065765b6bd91ac38bc1789b3d90b38
Sa1jer <dodikedward@gmail.com>
2026-09-05 23:55:46 +0500

    chore: release 1.3.64+3

--- lib/utils.dart
+++ lib/utils.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 
-const String kAppVersionLabel = 'v1.3.64+2';
+const String kAppVersionLabel = 'v1.3.64+3';
 
 /// Номер сборки из [kAppVersionLabel] — то, что стоит после `+`.
 ///
